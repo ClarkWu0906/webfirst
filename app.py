c6cb3ae278892106de25b50fc852f5b867d39483
ClarkWu0906 <clarkwu0906@gmail.com>
2023-04-18 12:06:56 +0000

    4/18

--- app.py
+++ app.py
@@ -18,21 +18,38 @@ def upload():
 def page2():
     return render_template('1G-POE.html')
 
+@app.route('/options', methods=['POST'])
+def options():
+    selected_option = request.form['1G_option']
+    if selected_option == 'option1':
+        return render_template('1G_mode1.html')
+    else:
+        return "Invalid option"
+
 @app.route("/page1")
 def page1():
     return render_template('page1.html')
 
-@app.route('/finished')
+@app.route('/finished', methods = ['POST', 'GET'])
 def finished():
-    return render_template('finished.html')
+        if request.method == 'POST':
+            return render_template('finished.html')
+        elif request.method == 'GET':
+            return render_template('finished.html')
+
 
 @app.route('/add')
 def add():
     return render_template('add.html')
 
-@app.route('/search')
+@app.route('/search' , methods = ['POST', 'GET'])
 def search():
-    return render_template('search.html')
+    if request.method == 'POST':
+            return render_template('search.html')
+    elif request.method == 'GET':
+        return render_template('search.html')
+
+    
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8888)
