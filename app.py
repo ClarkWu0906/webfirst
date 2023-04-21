9f8ab96b95e474b97b4f714adb7f3afcac44f91b
ClarkWu0906 <clarkwu0906@gmail.com>
2023-04-21 07:06:09 +0000

    4/21

--- app.py
+++ app.py
@@ -1,18 +1,41 @@
 from flask import Flask, request, jsonify, render_template
-
+import requests
+import json
+from json import load
+import os
+from werkzeug.utils import secure_filename
+from flask import session
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+UPLOAD_FOLDER = '/home/pdclab/WebServe/webfirst/upfile'
+ALLOWED_EXTENSIONS = set(['csv'])
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/upload', methods=['POST','GET'])
+@app.route('/upload', methods=['POST'])
 def upload():
     file = request.files['file']
     filename = file.filename
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    print(filename)
+    url = "http://140.115.52.21:11135/upload"
+
+    payload={}
+    files=[
+    ('file',(filename,open('/home/pdclab/WebServe/webfirst/upfile/'+filename,'rb'),'text/csv'))
+    ]
+    headers = {}
+
+    response = requests.request("POST", url, headers=headers, data=payload, files=files)
+
+    print(response.text)
     
-    file.save(filename)
     return jsonify({'result': '上傳成功', 'filename': filename})
+
 
 @app.route('/1G-POE')
 def page2():
@@ -26,30 +49,73 @@ def options():
     else:
         return "Invalid option"
 
-@app.route("/page1")
-def page1():
-    return render_template('page1.html')
-
 @app.route('/finished', methods = ['POST', 'GET'])
 def finished():
+        datas =[2, 2, 3, 4]
         if request.method == 'POST':
-            return render_template('finished.html')
+            # url = "http://140.115.52.21:11135/test"
+            # payload={}
+            # headers = {}
+            # response = requests.request("POST", url, headers=headers, data=payload)
+            # print(response.text)
+            return render_template('finished.html', data1 = datas)
         elif request.method == 'GET':
-            return render_template('finished.html')
+            return render_template('finished.html' , data1 = datas)
 
 
-@app.route('/add')
+@app.route('/add',methods = ['POST', 'GET'])
 def add():
-    return render_template('add.html')
+    if request.method == 'POST':
+        return render_template('add.html')
+    elif request.method == 'GET':
+         return render_template('add.html')
 
-@app.route('/search' , methods = ['POST', 'GET'])
+from flask import session
+
+@app.route('/search', methods=['POST', 'GET'])
 def search():
     if request.method == 'POST':
+        search_id = str(request.form.get("search_id"))
+        delete_id = request.form.get("delete_id")
+        if search_id:
+            print('search_id:', search_id)
+            url = "http://140.115.52.21:11135/search"
+
+            payload = json.dumps({
+                                "id": search_id
+                                })
+            headers = {
+                'Content-Type': 'application/json'
+            }
+
+            response = requests.request("POST", url, headers=headers, data=payload)
+
+            print(response.text)
+            data = json.loads(response.text)
+            return render_template('search.html', data=data)
+        elif delete_id:
+            print('delete_id:', delete_id)
+            url = "http://140.115.52.21:11135/deleteorder"
+
+            payload = json.dumps({
+                                "id": delete_id
+                                })
+            headers = {
+                'Content-Type': 'application/json'
+            }
+
+            response = requests.request("POST", url, headers=headers, data=payload)
+
+            print(response.text)
+            data = json.loads(response.text)
+            return render_template('search.html', message='Record deleted successfully.')
+        else:
             return render_template('search.html')
-    elif request.method == 'GET':
+    else:
         return render_template('search.html')
 
-    
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8888)
